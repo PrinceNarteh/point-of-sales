d602943c77865ff78e9e0da9a0d2aa767907c18f
PrinceNarteh <prinarteh@gmail.com>
2021-08-25 21:18:25 +0000

    Add ConfigModule and global validation pipe

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), ProductsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
