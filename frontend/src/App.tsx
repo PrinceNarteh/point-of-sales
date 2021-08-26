2d9bc9a50255226753ba6fde5502fef5ae8bc3b7
PrinceNarteh <prinarteh@gmail.com>
2021-08-26 08:56:25 +0000

    Add AdminLTE Template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,296 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SideBar from "./components/sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      {/* Navbar */}
+      <nav className="main-header navbar navbar-expand navbar-white navbar-light">
+        {/* Left navbar links */}
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              data-widget="pushmenu"
+              href="#"
+              role="button"
+            >
+              <i className="fas fa-bars" />
+            </a>
+          </li>
+          <li className="nav-item d-none d-sm-inline-block">
+            <a href="../../index3.html" className="nav-link">
+              Home
+            </a>
+          </li>
+          <li className="nav-item d-none d-sm-inline-block">
+            <a href="#" className="nav-link">
+              Contact
+            </a>
+          </li>
+        </ul>
+        {/* Right navbar links */}
+        <ul className="navbar-nav ml-auto">
+          {/* Navbar Search */}
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              data-widget="navbar-search"
+              href="#"
+              role="button"
+            >
+              <i className="fas fa-search" />
+            </a>
+            <div className="navbar-search-block">
+              <form className="form-inline">
+                <div className="input-group input-group-sm">
+                  <input
+                    className="form-control form-control-navbar"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+                  <div className="input-group-append">
+                    <button className="btn btn-navbar" type="submit">
+                      <i className="fas fa-search" />
+                    </button>
+                    <button
+                      className="btn btn-navbar"
+                      type="button"
+                      data-widget="navbar-search"
+                    >
+                      <i className="fas fa-times" />
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </li>
+          {/* Messages Dropdown Menu */}
+          <li className="nav-item dropdown">
+            <a className="nav-link" data-toggle="dropdown" href="#">
+              <i className="far fa-comments" />
+              <span className="badge badge-danger navbar-badge">3</span>
+            </a>
+            <div className="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+              <a href="#" className="dropdown-item">
+                {/* Message Start */}
+                <div className="media">
+                  <img
+                    src="../../dist/img/user1-128x128.jpg"
+                    alt="User Avatar"
+                    className="img-size-50 mr-3 img-circle"
+                  />
+                  <div className="media-body">
+                    <h3 className="dropdown-item-title">
+                      Brad Diesel
+                      <span className="float-right text-sm text-danger">
+                        <i className="fas fa-star" />
+                      </span>
+                    </h3>
+                    <p className="text-sm">Call me whenever you can...</p>
+                    <p className="text-sm text-muted">
+                      <i className="far fa-clock mr-1" /> 4 Hours Ago
+                    </p>
+                  </div>
+                </div>
+                {/* Message End */}
+              </a>
+              <div className="dropdown-divider" />
+              <a href="#" className="dropdown-item">
+                {/* Message Start */}
+                <div className="media">
+                  <img
+                    src="../../dist/img/user8-128x128.jpg"
+                    alt="User Avatar"
+                    className="img-size-50 img-circle mr-3"
+                  />
+                  <div className="media-body">
+                    <h3 className="dropdown-item-title">
+                      John Pierce
+                      <span className="float-right text-sm text-muted">
+                        <i className="fas fa-star" />
+                      </span>
+                    </h3>
+                    <p className="text-sm">I got your message bro</p>
+                    <p className="text-sm text-muted">
+                      <i className="far fa-clock mr-1" /> 4 Hours Ago
+                    </p>
+                  </div>
+                </div>
+                {/* Message End */}
+              </a>
+              <div className="dropdown-divider" />
+              <a href="#" className="dropdown-item">
+                {/* Message Start */}
+                <div className="media">
+                  <img
+                    src="../../dist/img/user3-128x128.jpg"
+                    alt="User Avatar"
+                    className="img-size-50 img-circle mr-3"
+                  />
+                  <div className="media-body">
+                    <h3 className="dropdown-item-title">
+                      Nora Silvester
+                      <span className="float-right text-sm text-warning">
+                        <i className="fas fa-star" />
+                      </span>
+                    </h3>
+                    <p className="text-sm">The subject goes here</p>
+                    <p className="text-sm text-muted">
+                      <i className="far fa-clock mr-1" /> 4 Hours Ago
+                    </p>
+                  </div>
+                </div>
+                {/* Message End */}
+              </a>
+              <div className="dropdown-divider" />
+              <a href="#" className="dropdown-item dropdown-footer">
+                See All Messages
+              </a>
+            </div>
+          </li>
+          {/* Notifications Dropdown Menu */}
+          <li className="nav-item dropdown">
+            <a className="nav-link" data-toggle="dropdown" href="#">
+              <i className="far fa-bell" />
+              <span className="badge badge-warning navbar-badge">15</span>
+            </a>
+            <div className="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+              <span className="dropdown-item dropdown-header">
+                15 Notifications
+              </span>
+              <div className="dropdown-divider" />
+              <a href="#" className="dropdown-item">
+                <i className="fas fa-envelope mr-2" /> 4 new messages
+                <span className="float-right text-muted text-sm">3 mins</span>
+              </a>
+              <div className="dropdown-divider" />
+              <a href="#" className="dropdown-item">
+                <i className="fas fa-users mr-2" /> 8 friend requests
+                <span className="float-right text-muted text-sm">12 hours</span>
+              </a>
+              <div className="dropdown-divider" />
+              <a href="#" className="dropdown-item">
+                <i className="fas fa-file mr-2" /> 3 new reports
+                <span className="float-right text-muted text-sm">2 days</span>
+              </a>
+              <div className="dropdown-divider" />
+              <a href="#" className="dropdown-item dropdown-footer">
+                See All Notifications
+              </a>
+            </div>
+          </li>
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              data-widget="fullscreen"
+              href="#"
+              role="button"
+            >
+              <i className="fas fa-expand-arrows-alt" />
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              data-widget="control-sidebar"
+              data-slide="true"
+              href="#"
+              role="button"
+            >
+              <i className="fas fa-th-large" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+      {/* /.navbar */}
+      {/* Main Sidebar Container */}
+      <SideBar />
+
+      {/* Content Wrapper. Contains page content */}
+      <div className="content-wrapper">
+        {/* Content Header (Page header) */}
+        <section className="content-header">
+          <div className="container-fluid">
+            <div className="row mb-2">
+              <div className="col-sm-6">
+                <h1>Fixed Layout</h1>
+              </div>
+              <div className="col-sm-6">
+                <ol className="breadcrumb float-sm-right">
+                  <li className="breadcrumb-item">
+                    <a href="#">Home</a>
+                  </li>
+                  <li className="breadcrumb-item">
+                    <a href="#">Layout</a>
+                  </li>
+                  <li className="breadcrumb-item active">Fixed Layout</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          {/* /.container-fluid */}
+        </section>
+        {/* Main content */}
+        <section className="content">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-12">
+                {/* Default box */}
+                <div className="card">
+                  <div className="card-header">
+                    <h3 className="card-title">Title</h3>
+                    <div className="card-tools">
+                      <button
+                        type="button"
+                        className="btn btn-tool"
+                        data-card-widget="collapse"
+                        title="Collapse"
+                      >
+                        <i className="fas fa-minus" />
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-tool"
+                        data-card-widget="remove"
+                        title="Remove"
+                      >
+                        <i className="fas fa-times" />
+                      </button>
+                    </div>
+                  </div>
+                  <div className="card-body">
+                    Start creating your amazing application!
+                  </div>
+                  {/* /.card-body */}
+                  <div className="card-footer">Footer</div>
+                  {/* /.card-footer*/}
+                </div>
+                {/* /.card */}
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* /.content */}
+      </div>
+      {/* /.content-wrapper */}
+      <footer className="main-footer">
+        <div className="float-right d-none d-sm-block">
+          <b>Version</b> 3.1.0
+        </div>
+        <strong>
+          Copyright © 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.
+        </strong>{" "}
+        All rights reserved.
+      </footer>
+      {/* Control Sidebar */}
+      <aside className="control-sidebar control-sidebar-dark">
+        {/* Control sidebar content goes here */}
+      </aside>
+      {/* /.control-sidebar */}
     </div>
   );
 }
