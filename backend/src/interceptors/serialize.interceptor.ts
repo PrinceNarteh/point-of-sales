28e8389c0d762372baff4e6bd4da86e027c8b5ca
PrinceNarteh <prinarteh@gmail.com>
2021-08-27 15:37:39 +0000

    Add Serialze interceptor

---
+++ backend/src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,16 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
