8ff30242922c944a8f4ac8016aa8b4c18bc23508
PrinceNarteh <prinarteh@gmail.com>
2021-08-27 02:47:15 +0000

    Add TypeORM and Sqlite

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ProductsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+    ProductsModule,
+  ],
   controllers: [],
   providers: [],
 })
