c1698e4d5dc4a43eb3ac772d2a25ceff666b17d5
PrinceNarteh <prinarteh@gmail.com>
2021-08-27 03:54:52 +0000

    Add User controller and CreateUserDto

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  @Get()
+  findAllUsers() {
+    return 'All Users';
+  }
+
+  @Get(':id')
+  findUser(@Param('id') id: string) {
+    return 'Single User';
+  }
+
+  @Post()
+  createUSer(@Body() body: CreateUserDto) {
+    return 'User Created.';
+  }
+}
